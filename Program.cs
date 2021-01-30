8c43563e127ce12dddabf28eac19b3f76564f688
nurcanozturk <44904765+nurcanozturk@users.noreply.github.com>
2021-01-30 18:16:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Methodlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Product product1 = new Product();
+            product1.Adi = "Elma";
+            product1.Fiyatı = 15;
+            product1.Aciklama = "Amasya Elması";
+
+            Product product2 = new Product();
+            product2.Adi = "Karpuz";
+            product2.Fiyatı = 25;
+            product2.Aciklama = "diyarbakır Karpuzu";
+
+            Product[] products = new Product[] { product1, product2 };
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.Adi);
+                Console.WriteLine(product.Fiyatı);
+                Console.WriteLine(product.Aciklama);
+                Console.WriteLine("-----------------");
+            }
+
+            Console.WriteLine("--------Methodlar------");
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(product1);
+            sepetManager.Ekle(product2);
+
+            sepetManager.Ekle2("armut","yesıl armut",12,45);
+            sepetManager.Ekle2("elma","yesil elma",12,14);
+            sepetManager.Ekle2("karpuz","diyarbakır",12,4);
+
+
+
+
+        }
+    }
+}
+
+
+
---
+++ SepetManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methodlar
+{
+    class SepetManager
+    {
+        public void Ekle(Product product)
+        {
+            Console.WriteLine("Sepete eklendi:" + product.Adi);
+
+        }
+        public void Ekle2(string productAdi, string Aciklama, double fiyat, int StokAdedi)
+        {
+            Console.WriteLine("Sepete eklendi:" + productAdi);
+
+
+        }
+    }
+}
